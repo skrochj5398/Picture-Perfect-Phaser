584b53f8cb778643a4ad25419d881de610eb59f2
JudgeXiong1 <78939340+JudgeXiong1@users.noreply.github.com>
2025-02-26 13:33:28 -0600

    maybe finally resolving merge chaos?

--- src/scenes/Prototype.js
+++ src/scenes/Prototype.js
@@ -26,6 +26,7 @@ class PrototypeScene extends Phaser.Scene {
       CONFIG.DEFAULT_WIDTH / cowFarm.width ,
       CONFIG.DEFAULT_HEIGHT / cowFarm.height
     )
+    this.add.image(200, 750, 'BuffaloSticker')
   }
 
   handleBlueBoxPointerDown (pointer) {
@@ -49,21 +50,6 @@ class PrototypeScene extends Phaser.Scene {
     //move the image to RedBox
     console.log(silhouette.x, silhouette.y)
     sticker.setPosition(silhouette.x, silhouette.y)
-   const BlueBox = this.add.image(200, 200, 'BlueBox').setInteractive()
-    this.add.image(300, 300, 'RedBox')
-    const cowFarm = this.add.image(CONFIG.DEFAULT_WIDTH / 2, CONFIG.DEFAULT_HEIGHT / 2, 'CowFarm')
-    cowFarm.setScale(
-      CONFIG.DEFAULT_WIDTH / cowFarm.width ,
-      CONFIG.DEFAULT_HEIGHT / cowFarm.height
-    )
-
-    this.add.image(200, 750, 'BuffaloSticker')
-
-
-    BlueBox.on('pointerdown', function(pointer){
-      //clicking... probably
-      console.log('clicked!')
-    })
   }
 
   
