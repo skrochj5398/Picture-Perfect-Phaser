5ef766ed6bbfc610040b27034c4b939d3e4a5801
skrochj5398 <skrochj5398@my.uwstout.edu>
2025-04-14 13:56:02 -0500

    working slider

--- src/models/Slider.js
+++ src/models/Slider.js
@@ -1,14 +1,66 @@
 import Phaser from 'phaser'
 
-class Slider extends Phaser.GameObjects.Container {
-  constructor (scene, x, y, handleTexture) {
-    super(scene, x, y)
+class Slider extends Phaser.GameObjects.Image {
+  constructor (scene, x, y, handleTexture, barTexture, barWidth, barHeight, fillTexture, min, max) {
+    // construct object
+    super(scene, x, y, handleTexture)
+    this.barWidth = barWidth
+    this.barHeight = barHeight
+    this.bounds = barWidth / 2
+    this.min = min
+    this.max = max
+    this.value = (max + min) / 2
+
+    // add to scene
     scene.add.existing(this)
-    this.setSize(120, 80)
+    // make handle draggable
     this.setInteractive({ draggable: true })
-    const handle = scene.add.image(0, 0, handleTexture)
-    this.add(handle)
-    this.on('drag', (pointer, dragX, dragY) => this.setPosition(dragX, dragY))
+    // add drag function
+    this.on('drag', (pointer, dragX, dragY) => {
+      // move handle
+      this.setPosition(Math.min(Math.max(dragX, x - this.bounds), x + this.bounds), this.y)
+      // mafs
+      const leftBound = x - this.bounds
+      const distanceToHandle = this.x - leftBound
+      const fillScale = distanceToHandle / scene.textures.getFrame(fillTexture).width
+      // set x scale for fill
+      fill.setScale(fillScale, 1)
+      const newXPos = distanceToHandle / 2 + leftBound
+      // set position so fill starts at left side of bar
+      fill.setPosition(newXPos, fill.y)
+      // set value... but mafs first
+      const totalDistance = x + this.bounds - leftBound
+      const fillPercent = distanceToHandle / totalDistance
+      console.log(fillPercent)
+      // set value
+      this.value = (max - min) * fillPercent + min
+      console.log(this.value)
+    })
+    console.log(x + this.bounds)
+    // make in front of everything else (bar should be 10)
+    this.setDepth(12)
+
+    // create bar
+    const bar = scene.add.image(x, y, barTexture)
+    // set depth
+    bar.setDepth(10)
+    // change scale to match width and height
+    bar.setScale(
+      barWidth / scene.textures.getFrame(barTexture).width,
+      barHeight / scene.textures.getFrame(barTexture).height
+    )
+
+    // create fill
+    const fill = scene.add.image(x, y, fillTexture)
+    // set depth 11
+    fill.setDepth(11)
+    // change scale and position to match starting position
+    const leftBound = x - this.bounds
+    const distanceToHandle = this.x - leftBound
+    const fillScale = distanceToHandle / scene.textures.getFrame(fillTexture).width
+    fill.setScale(fillScale, 1)
+    const newXPos = distanceToHandle / 2 + leftBound
+    fill.setPosition(newXPos, fill.y)
   }
 }
 
--- src/scenes/JessieTestScene.js
+++ src/scenes/JessieTestScene.js
@@ -5,14 +5,29 @@ import Slider from '../models/Slider.js'
 class JessieTestScene extends Phaser.Scene {
   preload () {
     // this is where to load images
-    this.load.image('optionsBackground', 'assets/UI/UI_Options_Claire_4_9_2025_v1.png')
+    this.load.image('optionsButton', 'assets/UI/UI_Options_Claire_4_9_2025_v1.png')
+    this.load.image('optionsBackground', '')
     this.load.image('optionsSliderBar', 'assets/UI/UI_Options_Menu_Bar_Off_Claire_4_9_2025_v1.png')
     this.load.image('optionsSliderFill', 'assets/UI/UI_Options_Menu_Bar_On_Claire_4_9_2025_v1.png')
     this.load.image('optionsSliderHandle', 'assets/UI/UI_Options_Menu_Slider_Claire_4_9_2025_v1.png')
   }
 
   create () {
-    this.optionsSlider = new Slider(this, CONFIG.DEFAULT_WIDTH / 2, CONFIG.DEFAULT_HEIGHT / 2, 'optionsSliderHandle')
+
+    // create button to bring up menu
+    this.optionsButton = new HoverableButton(this, 0, 0, )
+
+    // create slider
+    /*this.optionsSlider = new Slider(
+      this, CONFIG.DEFAULT_WIDTH / 2,
+      CONFIG.DEFAULT_HEIGHT / 2,
+      'optionsSliderHandle',
+      'optionsSliderBar',
+      this.textures.getFrame('optionsSliderBar').width,
+      this.textures.getFrame('optionsSliderBar').height,
+      'optionsSliderFill',
+      0, 100
+    )*/
   }
 }
 
