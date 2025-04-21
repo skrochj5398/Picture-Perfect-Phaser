160dac49f36bc97c25789cb50efe8bdb61d91ab7
MMoore33 <180556101+MMoore33@users.noreply.github.com>
2025-04-21 13:27:02 -0500

    Somewhat fixed curtain animation, but not entirely

--- src/scenes/WinScene.js
+++ src/scenes/WinScene.js
@@ -18,7 +18,7 @@ class WinScene extends Phaser.Scene {
       key: "Curtains",
       frames: this.anims.generateFrameNumbers("WinScreenAnim", {frames:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]}),
       frameRate: 12,
-      repeat: -1
+      repeat: 0
     })
 
     this.Curtain = this.add.sprite(CONFIG.DEFAULT_WIDTH / 2.0, CONFIG.DEFAULT_HEIGHT / 2.0, "WinScreenAnim")
@@ -34,6 +34,9 @@ class WinScene extends Phaser.Scene {
     // increment time?
     this.timeElapsed = this.game.getTime() - this.timeCheck
     console.log(this.timeElapsed)
+    // if (this.timeElapsed >= 2400){
+    //   this.Curtain.play("Curtains", false)
+    // }
     // check if enough time has passed
     if (this.timeElapsed >= 3000) {
       this.game.scene.stop('WinScene')
