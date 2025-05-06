ee5483fdcdee640eb9ea5f03390f802a795381e7
skrochj5398 <skrochj5398@my.uwstout.edu>
2025-05-06 18:26:54 -0500

    fixing merging conflicts

--- src/scenes/WinScene.js
+++ src/scenes/WinScene.js
@@ -27,7 +27,6 @@ class WinScene extends Phaser.Scene {
   }
 
   create () {
-
     // get levelIndex
     for (const level of this.data.levels) {
       if (level.name === this.levelCompleted) {
@@ -35,6 +34,17 @@ class WinScene extends Phaser.Scene {
         console.log('found level index: ', this.data.levels.indexOf(level))
         break
       }
+    }
+    this.add.image(CONFIG.DEFAULT_WIDTH / 2.0, CONFIG.DEFAULT_HEIGHT / 2.0, 'WinScreen')
+    // adding image based on user score
+    if (CONFIG.timesClicked <= this.data.levels[this.levelIndex].GoldRating) {
+      this.add.image(CONFIG.DEFAULT_WIDTH / 2.0, CONFIG.DEFAULT_HEIGHT / 2.0, 'GoldScreen')
+    }
+    else if (CONFIG.timesClicked <= this.data.levels[this.levelIndex].SilverRating) {
+      this.add.image(CONFIG.DEFAULT_WIDTH / 2.0, CONFIG.DEFAULT_HEIGHT / 2.0, 'SilverScreen')
+    }
+    else {
+      this.add.image(CONFIG.DEFAULT_WIDTH / 2.0, CONFIG.DEFAULT_HEIGHT / 2.0, 'BronzeScreen')
     }
 
     // create buttons
@@ -71,6 +81,19 @@ class WinScene extends Phaser.Scene {
     // play transition close
     console.log('play transition')
     this.transition.play({ key: 'Curtains', startFrame: 23 }, true)
+
+    // TESTING PURPOSES ONLY: Printing times clicked and then resetting variable
+    // console.log('Times Clicked: ' + CONFIG.timesClicked)
+    // CONFIG.timesClicked = 0;
+    //this.game.time.events.add(Phaser.Timer.SECOND * 2, this.announceScore, this);
+    this.announceScore()
+  }
+
+  announceScore () {
+    // TESTING PURPOSES ONLY: Printing times clicked and then resetting variable
+    console.log('Times Clicked: ' + CONFIG.timesClicked)
+    console.log('Gold Rating: ' + this.data.levels[this.levelIndex].GoldRating)
+    CONFIG.timesClicked = 0;
   }
 
   startTransition (targetScene, data = {}) {
